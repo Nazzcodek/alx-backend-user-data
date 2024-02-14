a842f99585c08afc1bb9e3fddbe1ec12661711ad
Nazzcodek <bellnas09@gmail.com>
2024-02-14 11:08:46 +0000

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,20 @@ class Auth:
 
     def require_auth(self,  path: str, excluded_paths: List[str]) -> bool:
         """required auth method"""
-        return False
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        for excluded_path in excluded_paths:
+            # Normalize paths to ensure they end with '/'
+            if not path.endswith('/'):
+                path += '/'
+            if not excluded_path.endswith('/'):
+                excluded_path += '/'
+
+            if path == excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization header method"""
