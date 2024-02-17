82138034e9319f0f944b3487f0f4beda0b3370ff
Nazzcodek <bellnas09@gmail.com>
2024-02-17 11:27:24 +0000

    check

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -15,6 +15,50 @@ def filter_datum(fields: List[str], redaction: str,
     return log_msg
 
 
+def get_logger() -> logging.Logger:
+    """logger getter method"""
+    log_user_data = logging.getLogger('user_data')
+    log_user_data.setLevel(logging.INFO)
+    log_user_data.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    log_user_data.addHandler(stream_handler)
+
+    return log_user_data
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ connect to database"""
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    cursor = mysql.connector.connection.MySQLConnection(user=username,
+                                                        password=password,
+                                                        host=host,
+                                                        database=db_name)
+    return cursor
+
+
+def main():
+    """ main module """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    field_names = [i[0] for i in cursor.description]
+
+    logger = get_logger()
+
+    for row in cursor:
+        str_row = ''.join(f'{f}={str(r)}; ' for r, f in zip(row, field_names))
+        logger.info(str_row.strip())
+
+    cursor.close()
+    db.close()
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
