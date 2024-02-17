f97e9a61cfbe79fcb7ddfcf2d569a46378fb825b
Nazzcodek <bellnas09@gmail.com>
2024-02-17 10:57:31 +0000

    check

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,13 +2,34 @@
 """filter logger module"""
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """this is a filter datum module"""
     for field in fields:
-        regex = "{}=[^{}]*".format(field, re.escape(separator))
-        log_msg = re.sub(regex, "{}={}".format(field, redaction), message)
-
+        regex = "{}=.*?{}".format(field, separator)
+        log_msg = re.sub(regex, "{}={}{}".format(field, redaction, separator),
+                         message)
     return log_msg
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        """init method"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format method"""
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
