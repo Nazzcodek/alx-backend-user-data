ad95e6a470933962ecbf6aa4a6cd91e43c60c804
Nazzcodek <bellnas09@gmail.com>
2024-02-17 10:39:13 +0000

    filtered logging

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""filter logger module"""
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """this is a filter datum module"""
+    for field in fields:
+        regex = "{}=[^{}]".format(field, re.escape(separator))
+        log_msg = re.sub(regex, "{}={}".format(field, redaction), message)
+
+    return log_msg
