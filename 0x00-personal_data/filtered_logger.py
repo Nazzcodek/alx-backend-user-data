660f9027a73637dba5e2b4cbf2afdedce5a53fe1
Nazzcodek <bellnas09@gmail.com>
2024-02-17 10:43:53 +0000

    check

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,7 +8,7 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """this is a filter datum module"""
     for field in fields:
-        regex = "{}=[^{}]*".format(field, separator)
+        regex = "{}=[^{}]*".format(field, re.escape(separator))
         log_msg = re.sub(regex, "{}={}".format(field, redaction), message)
 
     return log_msg
