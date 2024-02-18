d0bc61b77a5b28e4b7ba49d8158ec6e5b22f69ba
Nazzcodek <bellnas09@gmail.com>
2024-02-18 10:37:57 +0000

    session auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env/ python3
+"""sessiobn authentication module"""
+from api.v1.auth import Auth
+
+
+class SessionAuth(Auth):
+    """this is the session authentication object instances"""
+    pass
