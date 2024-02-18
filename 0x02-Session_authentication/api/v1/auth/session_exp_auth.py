e01dc80a9aa13927ae75e4dbe89c5799ff210d50
Nazzcodek <bellnas09@gmail.com>
2024-02-18 12:36:44 +0000

    session expiration authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""module for session expiration"""
+from api.v1.auth.session_auth import SessionAuth
+from datetime import datetime, timedelta
+from models.user import User
+import os
+
+
+class SessionExpAuth(SessionAuth):
+    """session expiration object instance"""
+
+    def __init__(self):
+        """initializaton method"""
+        SESSION_DURATION = os.environ.get('SESSION_DURATION')
+
+        try:
+            session_duration = int(SESSION_DURATION)
+        except Exception:
+            session_duration = 0
+
+        self.session_duration = session_duration
+
+    def create_session(self, user_id=None):
+        """create a session"""
+        session_id = super().create_session(user_id)
+
+        if session_id is None:
+            return None
+
+        session_dictionary = {
+            "user_id": user_id,
+            "created_at": datetime.now()
+        }
+
+        self.user_id_by_session_id[session_id] = session_dictionary
+
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """gets user_id from session_id"""
+
+        if session_id is None:
+            return None
+
+        if session_id not in self.user_id_by_session_id.keys():
+            return None
+
+        session_dictionary = self.user_id_by_session_id.get(session_id)
+
+        if session_dictionary is None:
+            return None
+
+        if self.session_duration <= 0:
+            return session_dictionary.get('user_id')
+
+        created_at = session_dictionary.get('created_at')
+
+        if created_at is None:
+            return None
+
+        expired_time = created_at + timedelta(seconds=self.session_duration)
+
+        if expired_time < datetime.now():
+            return None
+
+        return session_dictionary.get('user_id')
